1488c33d41135b253850c72ee8f23e63483e037a
sacredgeometry <lostrecordsuk@gmail.com>
2015-01-24 16:22:28 +0000

    Cleanup Project

--- Dev/Assets/Scripts/Debugger.cs
+++ Dev/Assets/Scripts/Debugger.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using BJW.Helpers;
+using BJW.Extensions;
 
 public class Debugger : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		var tet = new BJW.MyClass ();
 		GeneralHelper.Init();
 	}
 	
