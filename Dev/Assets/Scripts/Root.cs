c2d625dce1f3e745eb4881bc114d2bc8d5e86c29
sacredgeometry <lostrecordsuk@gmail.com>
2015-01-24 15:28:46 +0000

    Generated a test level and removed noise plugin
    I removed the noise plugin because there are build issues that need looking at, I generated a high res (2048) black and white image and can worry about random level generation later.

--- Dev/Assets/Scripts/Root.cs
+++ Dev/Assets/Scripts/Root.cs
@@ -2,18 +2,22 @@
 using System.Collections;
 using GGJ15.Helpers;
 
-namespace Tools
+namespace GGJ15
 {
 	public static class Root 
 	{
 		public static Vector3 origin { get; set; }
 	}
 }
+
 public class Root : MonoBehaviour {
 
-
 	void Awake () {
-		Tools.Root.origin = this.transform.position;
+		GGJ15.Root.origin = this.transform.position;
 	}
 
+	void Start () {
+
+
+	}
 }
