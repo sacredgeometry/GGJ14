57ac62c9116fe57b1d9d752ce55681d658f0d05a
sacredgeometry <lostrecordsuk@gmail.com>
2015-01-24 02:13:40 +0000

    Setup Project

---
+++ Dev/Assets/Scripts/Helpers/GeneralHelper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GGJ15.Helpers
+{
+	public static class GeneralHelper
+	{
+		public static void Init ()
+		{
+
+		}
+	}
+}
---
+++ Dev/Assets/Scripts/Root.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using GGJ15.Helpers;
+
+public class Root : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GeneralHelper.Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
