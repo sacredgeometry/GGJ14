4fc0053ae1806d3636c220936d50cf8f5bb1f488
sacredgeometry <lostrecordsuk@gmail.com>
2015-01-24 02:27:11 +0000

    Add Static Root Class

--- Dev/Assets/Scripts/Root.cs
+++ Dev/Assets/Scripts/Root.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 using GGJ15.Helpers;
 
+namespace Tools
+{
+	public static class Root 
+	{
+		public static Vector3 origin { get; set; }
+	}
+}
 public class Root : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		GeneralHelper.Init ();
+	void Awake () {
+		Tools.Root.origin = this.transform.position;
 	}
 	
 	// Update is called once per frame
